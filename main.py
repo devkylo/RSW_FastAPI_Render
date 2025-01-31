228bc2924e02c45fdd1a79d4a52626cda42222bf
Kylo <137659621+devkylo@users.noreply.github.com>
2025-01-31 14:23:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,11 @@ def root():
 
 @app.get("/get-schedule", response_model=ScheduleData)
 def get_schedule(team: str, date: str):
-    # JSON 파일 경로 설정
     json_file_path = os.path.join("team_today_schedules", team, date[:7], f"{date}_schedule.json")
+    print(f"JSON 파일 경로: {json_file_path}")
     
     if not os.path.exists(json_file_path):
+        print("❌ JSON 파일이 존재하지 않습니다.")
         return {"date": date, "day_shift": [], "night_shift": [], "vacation_shift": []}
     
     with open(json_file_path, "r", encoding="utf-8") as f:
